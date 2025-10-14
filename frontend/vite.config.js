0b2561f710e3ca55381809bd3fbff9696f0cac70
micruel051296 <micruel051296@gmail.com>
2025-10-14 12:35:47 +0000

    Fix: Configure project for Cloud Run deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env variables based on the mode (development, production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      // Expose the backend URL to the client-side code
+      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.BACKEND_URL || 'http://127.0.0.1:5000')
+    }
+  }
 })
